1ff3fb0261939049cd528d8604973bdcbe878d83
3773 <3773@kiwontech.com>
2021-10-20 15:45:13 +0900

    [Log4j2 설정]

--- src/main/java/com/example/jpa/main/MainController.java
+++ src/main/java/com/example/jpa/main/MainController.java
@@ -1,13 +1,18 @@
 package com.example.jpa.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController(value = "/")
 public class MainController {
 
+    final Logger logger = LogManager.getLogger(this.getClass());
+
     @GetMapping(value = "")
     public String main(){
+        logger.info("테스트");
         return "";
     }
 }
