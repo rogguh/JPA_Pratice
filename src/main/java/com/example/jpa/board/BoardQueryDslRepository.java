c47aab61033e24e78e5dd58bbbef843bdfd7428e
3773 <3773@kiwontech.com>
2021-12-02 10:22:53 +0900

    [     1. 동적쿼리 ]

--- src/main/java/com/example/jpa/board/BoardQueryDslRepository.java
+++ src/main/java/com/example/jpa/board/BoardQueryDslRepository.java
@@ -3,19 +3,31 @@ package com.example.jpa.board;
 import com.example.jpa.db.entity.BoardEntity;
 import com.example.jpa.domain.dto.BoardDTO;
 import com.example.jpa.domain.dto.PagingDTO;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.Visitor;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
+import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Optional;
 
 import static com.example.jpa.db.entity.QBoardEntity.boardEntity;
+import static org.springframework.util.StringUtils.*;
 
 @Repository
 @RequiredArgsConstructor
 public class BoardQueryDslRepository {
 
+    final Logger log = LogManager.getLogger(this.getClass());
     private final JPAQueryFactory queryFactory;
 
     /**
@@ -26,7 +38,9 @@ public class BoardQueryDslRepository {
     public long countAllByDto(BoardDTO boardDTO){
         return queryFactory.select(boardEntity.idx)
                            .from(boardEntity)
-                           .where(boardEntity.delStatus.eq(false))
+                           .where(
+                                   titleContains(boardDTO.getTitle())
+                           )
                            .fetchCount();
     }
 
@@ -45,7 +59,9 @@ public class BoardQueryDslRepository {
                                              , boardEntity.hits
                                   ))
                            .from(boardEntity)
-                           .where(boardEntity.delStatus.eq(false))
+                           .where(
+                                   titleContains(boardDTO.getTitle())
+                           )
                            .offset(boardDTO.getPageNo())
                            .limit(boardDTO.getPageSize())
                            .orderBy(boardEntity.regDate.desc())
@@ -63,4 +79,12 @@ public class BoardQueryDslRepository {
                 .fetchOne();
     }
 
+    /**
+     * FUNCTION :: 검색조건
+     * @param params
+     * @return
+     */
+    private BooleanExpression titleContains(String title){
+        return hasText(title) ? boardEntity.title.contains(title) : null;
+    }
 }
